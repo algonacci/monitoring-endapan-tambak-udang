db45e3c5d0e7dd50af344dc9f646146934df0f74
Eric Julianto <algonacci1@gmail.com>
2023-04-08 17:30:23 +0700

    Finishing

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
+import datetime
 import os
+import pickle
 
+import pandas as pd
 from flask import Flask, render_template, request
-from PIL import Image
+from sklearn.preprocessing import minmax_scale
 from werkzeug.utils import secure_filename
 
 import module as md
@@ -9,6 +12,7 @@ import module as md
 app = Flask(__name__)
 app.config['ALLOWED_EXTENSIONS'] = set(['png', 'jpg', 'jpeg'])
 app.config['UPLOAD_FOLDER'] = 'static/uploads/'
+app.config['MODEL_FILE'] = 'model/knn_model.pkl'
 
 
 def allowed_file(filename):
@@ -16,17 +20,52 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
 
 
+with open(app.config['MODEL_FILE'], 'rb') as file:
+    knn_model = pickle.load(file)
+
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
         image = request.files["image"]
+        pakan = request.form["pakan"]
+        usia = int(request.form["usia"])
         if image and allowed_file(image.filename):
             filename = secure_filename(image.filename)
             image.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
             image_path = os.path.join(app.config["UPLOAD_FOLDER"], filename)
             result = md.image_processing(image_path)
-            print(result)
-            return render_template("index.html")
+            df1 = pd.read_csv('static/result/rgb.csv')
+            df2 = pd.read_csv('static/result/first_order.csv')
+            df3 = pd.read_csv('static/result/second_order.csv')
+            df = pd.concat([df1, df2, df3], axis=1)
+            df.insert(0, "Pakan", pakan)
+            df.insert(1, "Usia", usia)
+            print(df)
+            df = pd.DataFrame(minmax_scale(df), columns=df.columns)
+            # print(result)
+            label = knn_model.predict(df)
+            print("====================")
+            print(label)
+
+            # Calculate shift_pond based on usia and predicted class
+            if usia <= 30:
+                shift_pond = 30 - usia
+                shift_date = datetime.date.today() + datetime.timedelta(days=shift_pond)
+            elif label == 1:
+                shift_pond = 4
+                shift_date = datetime.date.today() + datetime.timedelta(days=shift_pond)
+            elif label == 2:
+                shift_pond = 3
+                shift_date = datetime.date.today() + datetime.timedelta(days=shift_pond)
+            elif label == 3:
+                shift_pond = 2
+                shift_date = datetime.date.today() + datetime.timedelta(days=shift_pond)
+            else:
+                shift_pond = None
+                shift_date = None
+
+            return render_template("index.html", result=label, usia=usia, shift_pond=shift_pond, shift_date=shift_date)
         else:
             return render_template("index.html", error="Silahkan upload gambar dengan format JPG")
     else:
